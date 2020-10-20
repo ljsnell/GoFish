6d79569ad77aa26ea8af06568592906cb47cbb40
ljsnell <ljsnell@users.noreply.github.com>
2020-10-19 22:00:44 -0500

    Updated game ending code. (#1)
    Signed-off-by: ljsnell <ljsnell7@gmail.com>

--- Game.py
+++ Game.py
@@ -23,6 +23,12 @@ hand1, player1_pts = utilities.scoring_cycle(hand1, "Player1's score: ", player1
 hand2, player2_pts = utilities.scoring_cycle(hand2, "Player2's score: ", player2_pts)
 
 while 1 < 2:
+    # Check if deck's exhausted
+    if(len(deck_list) == 0):
+        print("Well played!")
+        print("Player 1 Final score:" + str(player1_pts))
+        print("Player 2 Final score:" + str(player2_pts))
+
     correct_guess = True
     while correct_guess == True:
         # P1 guess card
@@ -34,7 +40,10 @@ while 1 < 2:
 
         # Check if there's 4 in the hand
         hand1, player1_pts = utilities.scoring_cycle(hand1, "Player1's score: ", player1_pts)
-
+        
+        if(len(hand1) == 0):
+            hand1, deck_list = deck.draw_cards(deck_list, hand1, 1)
+        
         utilities.print_hands(hand1, hand2)
 
     correct_guess = True
@@ -47,6 +56,9 @@ while 1 < 2:
         hand2, hand1, deck_list, correct_guess = utilities.is_card_in_hand(hand2, hand1, p2_guess, deck_list, deck, current_player, correct_guess)
 
         # Check if there's 4 in the hand
+        
         hand2, player2_pts = utilities.scoring_cycle(hand2, "Player2's score: ", player2_pts)
+        if(len(hand1)== 0):
+            hand1, deck_list = deck.draw_cards(deck_list, hand1, 1)
 
         utilities.print_hands(hand1, hand2)
