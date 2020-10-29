c0b08d9af997a2cb71d747e6c171d0daca2e54e4
ljsnell <ljsnell7@gmail.com>
2020-10-28 21:25:34 -0500

    Signed-off-by: ljsnell <ljsnell7@gmail.com>

--- Mongo_Queries.py
+++ Mongo_Queries.py
@@ -14,7 +14,7 @@ class MongoClient:
         pickle_hand1 = pickle.dumps(p1_hand)
         pickle_hand2 = pickle.dumps(p2_hand)
         pickle_deck = pickle.dumps(deck)
-
+        # Game ID = # of documents in db + 1
         random_password = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
         game_state = { "p1_hand": pickle_hand1, "p2_hand": pickle_hand2, "player1_pts": p1_pts, 
             "player2_pts": p2_pts, "deck": pickle_deck, "game_id": game_id, "current_player": current_player,
@@ -29,3 +29,5 @@ class MongoClient:
         game_state = self.game_col.find_one()
         print(game_state)
         print(pickle.loads(game_state['p1_hand']))
+        # Use mongoDB compass.
+        # C:\Program Files\MongoDB\Server\4.4\bin\mongod.exe
